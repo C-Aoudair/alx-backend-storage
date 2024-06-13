fef072026c231473f6191c431d795d1faa2f04fa
C-Aoudair <aoudair16031999@gmail.com>
2024-06-13 12:11:18 +0100

    fix task 0: aadd if not exests constains to the table statment

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,9 @@
 -- This script creates a table users with a unique constraint on the email column
 
-CREATE TABLE IF NOT EXESTS users (
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255)
 );
+
+CREATE
