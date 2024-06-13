e1ff890adc446746f17d775032a7395af6a77d49
C-Aoudair <aoudair16031999@gmail.com>
2024-06-13 12:08:35 +0100

    add task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- This script creates a table users with a unique constraint on the email column
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255)
+);
