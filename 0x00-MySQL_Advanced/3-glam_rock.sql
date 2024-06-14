1f4b322547a9814c7b39933e591b03afe7b9e37e
C-Aoudair <aoudair16031999@gmail.com>
2024-06-14 12:59:32 +0100

    fix task 0

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,8 +3,6 @@
 SELECT 
     band_name, 
     (2022 - formed) - (CASE WHEN split != '0' THEN (2022 - CAST(split AS UNSIGNED)) ELSE 0 END) AS lifespan
-FROM 
-    metal_bands
-WHERE
-    style LIKE '%Glam rock%'
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
