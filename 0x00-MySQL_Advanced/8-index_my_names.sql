46b90f6fcb36d66c54138f701973147eaa844b25
C-Aoudair <aoudair16031999@gmail.com>
2024-06-15 14:28:23 +0100

    fix task 8: change the name of the index from inx_name_first to idx_name_first, there was a typo in idx

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 -- This script  creates an index idx_name_first on the table names and the first letter of name.
 
-CREATE INDEX inx_name_first ON names (name(1));
+CREATE INDEX idx_name_first ON names (name(1));
