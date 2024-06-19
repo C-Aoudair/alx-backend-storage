df101261955f97169a36f37630110d0c65cf86ea
C-Aoudair <aoudair16031999@gmail.com>
2024-06-19 12:33:25 +0100

    add condition to prevent the module from running when it is imporeted

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,17 +4,18 @@ This Module contains script that provides some stats about Nginx
 logs stored in MongoDB.
 """
 
-from pymongo import MongoClient
+if __name__ == "__main__":
+    from pymongo import MongoClient
 
-client = MongoClient("mongodb://127.0.0.1:27017")
-logs = client.logs.nginx
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    logs = client.logs.nginx
 
-print(f"{logs.count_documents({})} logs")
-print(f"\tmethod GET: {logs.count_documents({'method': 'GET'})}")
-print(f"\tmethod POST: {logs.count_documents({'method': 'POST'})}")
-print(f"\tmethod PUT: {logs.count_documents({'method': 'PUT'})}")
-print(f"\tmethod PATCH: {logs.count_documents({'method': 'PATCH'})}")
-print(f"\tmethod DELETE: {logs.count_documents({'method': 'DELETE'})}")
-print("{} status check".format(
-    logs.count_documents({'method': 'GET', 'path': '/status'})
-))
+    print(f"{logs.count_documents({})} logs")
+    print(f"\tmethod GET: {logs.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {logs.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {logs.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {logs.count_documents({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {logs.count_documents({'method': 'DELETE'})}")
+    print("{} status check".format(
+        logs.count_documents({'method': 'GET', 'path': '/status'})
+    ))
