8393ed509c3c5b32f782f4b543af342fabe9b9ce
C-Aoudair <aoudair16031999@gmail.com>
2024-06-19 12:43:10 +0100

    make task 12 more concise and better design

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     logs = client.logs.nginx
 
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
     print(f"{logs.count_documents({})} logs")
     print("Methods:")
-    print(f"\tmethod GET: {logs.count_documents({'method': 'GET'})}")
-    print(f"\tmethod POST: {logs.count_documents({'method': 'POST'})}")
-    print(f"\tmethod PUT: {logs.count_documents({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {logs.count_documents({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {logs.count_documents({'method': 'DELETE'})}")
-    print("{} status check".format(
-        logs.count_documents({'method': 'GET', 'path': '/status'})
-    ))
+    for method in methods:
+        count = logs.count_documents({'method': method})
+        print(f"\tmethod {method}: {count}")
+    
+    statsCount = logs.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{statsCount} status check")
