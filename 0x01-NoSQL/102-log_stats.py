2cf12b7fded6ac825ecfab3907a473317814b19b
C-Aoudair <aoudair16031999@gmail.com>
2024-06-19 15:03:38 +0100

    add task 102

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+This Module contains script that provides some stats about Nginx
+logs stored in MongoDB.
+"""
+
+if __name__ == "__main__":
+    from pymongo import MongoClient
+
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    logs = client.logs.nginx
+
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+    print(f"{logs.count_documents({})} logs")
+    print("Methods:")
+    for method in methods:
+        count = logs.count_documents({'method': method})
+        print(f"\tmethod {method}: {count}")
+    
+    statsCount = logs.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{statsCount} status check")
+
+    pipeline = [
+        { "$group": { "_id": "$ip", "count": { "$sum": 1 } } },
+        { "$sort": { "count": -1 } },
+        {"$limit": 10 }
+    ]
+
+    print("IPs:")
+    IPs = logs.aggregate(pipeline)
+    for IP in IPs:
+        print(f"\t{IP['_id']}: {IP['count']}")
+
