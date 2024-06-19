04629ba4ca5a84ea63d1edbadfa6cbc44a81ca9f
C-Aoudair <aoudair16031999@gmail.com>
2024-06-19 11:47:53 +0100

    add task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" This Module contains script that provides some stats about Nginx
+    logs stored in MongoDB.
+"""
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://127.0.0.1:27017')
+logs = client.logs.nginx
+
+print(f"{logs.count_documents({})} logs")
+print(f"\tmethod GET: {logs.count_documents({'method': 'GET'})}")
+print(f"\tmethod POST: {logs.count_documents({'method': 'POST'})}")
+print(f"\tmethod PUT: {logs.count_documents({'method': 'PUT'})}")
+print(f"\tmethod PATCH: {logs.count_documents({'method': 'PATCH'})}")
+print(f"\tmethod DELETE: {logs.count_documents({'method': 'DELETE'})}")
+print(f"{logs.count_documents({'method': 'GET', 'path': '/status'})} status check")
