75067353c8b264c49043c7aa8155d963c0e68c4f
C-Aoudair <aoudair16031999@gmail.com>
2024-06-20 14:35:08 +0100

    update exercise.py: add three methods: get, get_str and get_int

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,3 +16,37 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn=None):
+        """
+        take a key string argument and an optional Callable
+        argument named fn. and return the value for the key
+        """
+        client = self._redis
+        value = client.get(key)
+
+        if not value:
+            return None
+
+        if fn == str:
+            return self.get_str(value)
+
+        if fn == int:
+            return self.get_int(value)
+
+        if callable(fn):
+            return fn(value)
+
+        return value
+
+    def get_str(self, value):
+        """
+        takes a value and return its correspondent str type
+        """
+        return value.decode('utf-8')
+
+    def get_int(self, value):
+        """
+        takes a value and return its correspondent int type
+        """
+        return int(value)
