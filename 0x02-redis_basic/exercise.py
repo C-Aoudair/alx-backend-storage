665301e06e63cd276e5b170bf982a66d73e17358
C-Aoudair <aoudair16031999@gmail.com>
2024-06-20 13:41:03 +0100

    fix exercise.py: add type anotation with Union and add flushdb method for __init__

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,15 +2,16 @@
 """ This module contains Cache class."""
 import redis
 import uuid
-from typing import Any
+from typing import Union
 
 
 class Cache():
     """ Cache class that create a basic cache system"""
     def __init__(self):
         self._redis = redis.Redis()
+        self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ takes a data argument and returns a string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
