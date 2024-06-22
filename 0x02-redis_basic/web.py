7ec35e2bdc6ecc2771d9cf031298ea9af902bb18
C-Aoudair <aoudair16031999@gmail.com>
2024-06-22 16:31:22 +0100

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,32 +7,35 @@ from functools import wraps
 from typing import Callable
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+cache = redis.Redis()
+"""
+The module-level Redis instance.
+"""
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
+def count(func: Callable) -> Callable:
+    """
+    Caches the output of fetched data.
+    """
+    @wraps(func)
+    def wrapper(url) -> str:
+        """
+        The wrapper function for caching the output.
+        """
+        cache.incr(f'count:{url}')
+        result = cache.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+        result = func(url)
+        cache.setex(f'result:{url}', 10, result)
         return result
-    return invoker
+    return wrapper
 
 
-@data_cacher
+@count
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's
+    """
+    Returns the content of a URL after caching the request's
     response, and tracking the request.
-    '''
+    """
     return requests.get(url).text
