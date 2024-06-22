4b9ef87e26b389aef1f4da87fac42281e4b1e8ae
C-Aoudair <aoudair16031999@gmail.com>
2024-06-22 16:22:33 +0100

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env python3
 """ This module contains get_page function."""
+
+from typing import Callable
 import redis
 import requests
 from functools import wraps
 
-cache = redis.Redis(host='localhost', port=6379, db=0)
+cache = redis.Redis(host="localhost", port=6379, db=0)
 
-def cache_page(func):
-    """ decorator function"""
+
+def cache_page(func: Callable) -> Callable:
+    """decorator function"""
+
     @wraps(func)
     def wrapper(url: str) -> str:
         cache_key = f"page:{url}"
         count_key = f"count:{url}"
-        
+
+        cache.incr(count_key)
+
         cached_page = cache.get(cache_key)
         if cached_page:
-            return cached_page.decode('utf-8')
-        
+            return cached_page.decode("utf-8")
+
         page_content = func(url)
-        
+
         cache.setex(cache_key, 10, page_content)
-        
-        cache.incr(count_key)
-        
+
         return page_content
-    
+
     return wrapper
 
 
